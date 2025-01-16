ec4d5282550541424f0b40be26c3743a4b6e628f
Marc LaFleur <marc@mechanicadvisor.com>
2025-01-15 23:09:37 -0500

    Bump packages

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,23 @@
 import * as core from '@actions/core'
-import { ArtifactDownloader } from './artifact-downloader';
+import { ArtifactDownloader } from './artifact-downloader'
 
 async function run(): Promise<void> {
-  
   try {
-    const buildDefinitionId: number = Number(core.getInput('buildDefinitionId'));
-    const projectId: string = core.getInput('projectId');
-    const patToken: string = core.getInput('patToken');
-    const orgName: string = core.getInput('orgName');
-    const artifactName: string = core.getInput('artifactName');
-    const artifactDownloader = new ArtifactDownloader();
-    artifactDownloader.download(projectId,buildDefinitionId,patToken,orgName,artifactName);
-  } catch (error) {
-    core.setFailed(error.message)
+    const buildDefinitionId: number = Number(core.getInput('buildDefinitionId'))
+    const projectId: string = core.getInput('projectId')
+    const patToken: string = core.getInput('patToken')
+    const orgName: string = core.getInput('orgName')
+    const artifactName: string = core.getInput('artifactName')
+    const artifactDownloader = new ArtifactDownloader()
+    artifactDownloader.download(
+      projectId,
+      buildDefinitionId,
+      patToken,
+      orgName,
+      artifactName
+    )
+  } catch (error: any) {
+    core.setFailed(error.message || 'Unknown error')
   }
 }
 
